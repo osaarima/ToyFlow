b13e7fafb300ab12e17300bf75595a4fe3c4d240
Oskari Saarimäki <oskari.antti.matti.saarimaki@cern.ch>
2019-04-17 16:08:00 +0300

    Cleaned run.sh and added comments.

--- run.sh
+++ run.sh
@@ -1,46 +1,66 @@
 #!/bin/bash
+###############################################
 
+###############################################
+# Usage and arguments
+###############################################
 PROG=`basename $0`
 if [ $# -ne 3 ]
 then
-    echo "Usage: $PROG comment ptDep weight"
+    echo "Usage: ${PROG} comment ptDep weight"
     exit;
 fi
 
-function setenv(){ export $1=$2; }
+###############################################
+# Script arguments
+###############################################
+export comment=$1
+export ptDep=$2
+export weight=$3
 
-setenv comment $1
-setenv ptDep $2
-setenv weight $3
-setenv noFileToRun 100
-setenv noEvents 1000
+###############################################
+# Run settings
+###############################################
+export noFileToRun=1
+export noEvents=1000
+export dNdeta=1000
 
-setenv DoWhat       toyFlow
-setenv oname        $DoWhat_${comment}
+###############################################
+# Program name
+###############################################
+export DoWhat=toyFlow
+export oname=${DoWhat}_${comment}
 
-setenv Main_DIR     `pwd`
-setenv Out_DIR      $Main_DIR/outputs/${oname}/data
-setenv LOG_DIR      $Main_DIR/outputs/${oname}/logs
-setenv OUT_ERRORS   $Main_DIR/outputs/${oname}/errors
+###############################################
+# Output file locations
+###############################################
+export Main_DIR=`pwd`
+export Out_DIR=${Main_DIR}/outputs/${oname}/data
+export LOG_DIR=${Main_DIR}/outputs/${oname}/logs
+export OUT_ERRORS=${Main_DIR}/outputs/${oname}/errors
+mkdir -p ${Out_DIR}
+mkdir -p ${OUT_ERRORS}
+mkdir -p ${LOG_DIR}
 
-mkdir -p $Out_DIR
-mkdir -p $OUT_ERRORS
-mkdir -p $LOG_DIR
-
-# simplify this !!!
-cat << EOF > exec_toyFlow_$comment
+###############################################
+# Create executible file
+###############################################
+cat << EOF > exec_toyFlow_${comment}
 #!/bin/bash -f
-cd $Main_DIR
+cd ${Main_DIR}
 source setup.sh
-export what=$DoWhat
+export what=${DoWhat}
 export sedN=1000
-export iseg=\$SLURM_ARRAY_TASK_ID
-sedN=\`expr \$sedN + \${iseg}\`
-export outfile=${comment}_\$sedN
-export Log=$LOG_DIR/$DoWhat-\$sedN.log
-./\${what} $noEvents 1000 $ptDep $weight \$outfile $Out_DIR/ \$sedN >& \$Log
-cd $Main_DIR
+export iseg=\${SLURM_ARRAY_TASK_ID}
+sedN=\`expr \${sedN} + \${iseg}\`
+export outfile=${comment}_\${sedN}
+export Log=${LOG_DIR}/${DoWhat}-\${sedN}.log
+./\${what} ${noEvents} ${dNdeta} ${ptDep} ${weight} \${outfile} ${Out_DIR}/ \${sedN} >& \${Log}
+cd ${Main_DIR}
 EOF
-#\${what} \$outfile \$sedN 5000000 >& \$Log
-chmod +x exec_dijet_$comment
-    sbatch -v --array=1-$noFileToRun exec_toyFlow_$comment -J $comment  -e $OUT_ERRORS -o $OUT_ERRORS
+###############################################
+# Make the file executable and run it in SLURM
+###############################################
+chmod +x exec_dijet_${comment}
+## If you want to test the run, add --test-only to the beginning
+sbatch -v --array=1-${noFileToRun} exec_toyFlow_${comment} -J ${comment}  -e ${OUT_ERRORS} -o ${OUT_ERRORS}
